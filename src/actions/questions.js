8b89490c222ac275156e7105cb9cb32f5ea84f47
Nick Renaud <nick_renaud@hotmail.com>
2021-01-23 10:34:15 -0800

    feat: action creators for questions and answers
    -actions to handle both saving new questions and answers for the
    questions.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,44 @@
+import { _saveQuestion, _saveQuestionAnswer } from '../utils/_DATA'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const SAVE_QUESTION = 'SAVE_QUESTION'
+export const SAVE_ANSWER = 'SAVE_ANSWER'
+
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+function addQuestion(question) {
+    return {
+        type: SAVE_QUESTION,
+        question
+    }
+}
+
+export function saveQuestion(question) {
+    return async (dispatch, getState) => {
+        const { authedUser } = getState()
+        const savedQuestion = await _saveQuestion({ question, author: authedUser })
+        dispatch(addQuestion(savedQuestion))
+    }
+}
+
+function saveAnswer(answer) {
+    return {
+        type: SAVE_ANSWER,
+        answer
+    }
+}
+
+export function saveQuestionAnswer({answer, id}) {
+    return async (dispatch, getState) => {
+        const { authedUser } = getState()
+        const questionAnswer = {answer, id, authedUser}
+        await _saveQuestionAnswer(questionAnswer)
+        dispatch(saveAnswer(questionAnswer))
     }
 }

310fcfffd58437710a5635c13d1c83562bc68a0d
Nick Renaud <nick_renaud@hotmail.com>
2021-01-26 15:30:55 -0800

    add custom color props

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#26a69a'
+      },
+      secondary: {
+          main: '#70c4bc',
+          dark: '#357f78'
+      },
+    },
+  })

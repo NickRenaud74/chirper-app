30f98993f3e59b90eaea236a07ea807f6cf0ef51
Nick Renaud <nick_renaud@hotmail.com>
2021-01-27 15:16:08 -0800

    add error color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,16 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#26a69a'
-      },
-      secondary: {
-          main: '#70c4bc',
-          dark: '#357f78'
-      },
+  palette: {
+    primary: {
+      main: '#009688'
     },
-  })
+    secondary: {
+      main: '#70c4bc',
+      dark: '#357f78'
+    },
+    error: {
+      main: '#fbc02d'
+    }
+  },
+})

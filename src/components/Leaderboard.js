c9df13b1c5c51bb4fe9fc5ce7f2bb12ca42c055b
Nick Renaud <nick_renaud@hotmail.com>
2021-02-01 23:13:18 -0800

    feat: add place icons

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,10 +1,82 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useStyles } from '../styles/cardStyles'
+import { Container, Card, Typography, Grid, Avatar, CardHeader, Table, TableBody, TableRow, TableCell } from '@material-ui/core'
+import LooksOneIcon from '@material-ui/icons/LooksOne'
+import LooksTwoIcon from '@material-ui/icons/LooksTwo'
+import Looks3Icon from '@material-ui/icons/Looks3'
 
 function Leaderboard() {
+    const users = useSelector(state => state.users)
+    const classes = useStyles()
+
+    const leaders = Object.keys(users).sort((a, b) => {
+        const scoreB = users[b].questions.length + Object.keys(users[b].answers).length
+        const scoreA = users[a].questions.length + Object.keys(users[a].answers).length
+        return scoreB - scoreA
+    })
+
+    const getPosition = index => {
+        switch (index) {
+            case 0:
+                return <LooksOneIcon/>
+            case 1:
+                return <LooksTwoIcon />
+            case 2:
+                return <Looks3Icon />
+            default: 
+                return null
+        }
+    }
+
     return (
-        <div>
-            <h1>leaderboard</h1>
-        </div>
+        <Container maxWidth='sm'>
+            <Typography variant='h2' className={classes.centerText}>Leaderboard</Typography>
+
+            {leaders.map((user, index) => (
+                <Card className={`${classes.root}`} raised key={users[user].id}>
+                    <Grid container>
+                        <Grid item xs={4} className={classes.avatarBox}>
+                            {getPosition(index)}
+                            <Avatar className={classes.avatarMed}>NR</Avatar>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <CardHeader title={users[user].name} />
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography variant='subtitle2'>
+                                                Answered Questions:
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            {Object.keys(users[user].answers).length}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography variant='subtitle2'>
+                                                Created Questions:
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            {users[user].questions.length}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Grid>
+                        <Grid item xs={2} className={classes.score}>
+                            <Avatar className={classes.avatarSm}>
+                                {users[user].questions.length + Object.keys(users[user].answers).length}
+                            </Avatar>
+                        </Grid>
+                    </Grid>
+                </Card>
+            ))}
+
+        </Container>
     )
 }
 

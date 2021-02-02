102e169ef51ce6e23ab8fd57c832af21d33acf35
Nick Renaud <nick_renaud@hotmail.com>
2021-02-01 23:08:44 -0800

    feat: add logic for login page
    -using state of loadingBar instead of authedUser to decide what to
    render
    -if authedUser has yet to be assigned redirect to the login page.
    -add route for login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { getData } from '../actions/shared'
 import { useDispatch, useSelector } from 'react-redux'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useHistory } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
+import { useStyles } from '../styles/cardStyles'
 import Nav from './Nav'
 import Leaderboard from './Leaderboard'
 import Dashboard from './Dashboard'
@@ -11,23 +12,31 @@ import { Container } from '@material-ui/core'
 import QuestionPoll from './QuestionPoll'
 import QuestionResults from './QuestionResults'
 import NewQuestion from './NewQuestion'
+import Login from './Login'
 
 function App() {
   const authedUser = useSelector(state => state.authedUser)
+  const loading = useSelector(state => state.loadingBar.default)
   const dispatch = useDispatch()
+  const classes = useStyles()
+  const history = useHistory()
 
-  const loading = authedUser === null
+  const loggedIn = authedUser !== null
 
   useEffect(() => {
     dispatch(getData())
   }, [dispatch])
 
+  if (!loggedIn) {
+   history.push('/login')
+  }
+
   return (
     <div className="App">
-      <LoadingBar />
+      <LoadingBar className={classes.loadingBar} />
       <Nav />
-      {loading ? null :       
-        <Container maxWidth='sm'>          
+      {loading !== 0 ? null :
+        <Container maxWidth='sm'>
           <Switch>
             <Route exact path='/'>
               <Dashboard />
@@ -39,10 +48,13 @@ function App() {
               <Leaderboard />
             </Route>
             <Route exact path='/questions/:questionId'>
-              <CardTemplate component={QuestionPoll}/>
+              <CardTemplate component={QuestionPoll} />
             </Route>
             <Route path='/questions/:questionId/results'>
               <CardTemplate component={QuestionResults} />
+            </Route>
+            <Route path='/login'>
+              <Login />
             </Route>
           </Switch>
         </Container>

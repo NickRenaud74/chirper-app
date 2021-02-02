7c8ecd8dba51f96d94828501dc049500c18b4371
Nick Renaud <nick_renaud@hotmail.com>
2021-02-01 23:16:36 -0800

    feat: handle login logic
    -update status of login/logout tab depending on whether the user is
    logged in or out

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { Tab, Tabs, AppBar } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 function Nav() {
     const [tab, setTab] = useState(0)
+    const authedUser = useSelector(state => state.authedUser)
+
+    const loggedIn = authedUser !== null
 
     function handleChange(e, newValue) {
         setTab(newValue)
@@ -18,7 +22,7 @@ function Nav() {
                 <Tab label='Home' component={Link} to='/' />
                 <Tab label='New Question' component={Link} to='/add' />
                 <Tab label='Leaderboard' component={Link} to='/leaderboard' />
-                <Tab label='Logout' component={Link} to='/' />
+                <Tab label={loggedIn ? 'Logout': 'Login'} component={Link} to='/Login' />
             </Tabs>
         </AppBar>
     )

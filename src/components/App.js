9c829c9dab00609b48f067c52a6ba3071b76b190
Nick Renaud <nick_renaud@hotmail.com>
2021-02-04 18:50:55 -0800

    refactor: use login to redirect routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Route, Switch, useHistory } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
 import { useStyles } from '../styles/cardStyles'
+import { Container } from '@material-ui/core'
 import Nav from './Nav'
 import Leaderboard from './Leaderboard'
 import Dashboard from './Dashboard'
 import CardTemplate from './CardTemplate'
-import { Container } from '@material-ui/core'
 import QuestionPoll from './QuestionPoll'
 import QuestionResults from './QuestionResults'
 import NewQuestion from './NewQuestion'
@@ -16,7 +16,6 @@ import Login from './Login'
 
 function App() {
   const authedUser = useSelector(state => state.authedUser)
-  const loading = useSelector(state => state.loadingBar.default)
   const dispatch = useDispatch()
   const classes = useStyles()
   const history = useHistory()
@@ -28,37 +27,35 @@ function App() {
   }, [dispatch])
 
   if (!loggedIn) {
-   history.push('/login')
+    history.push('/login')
   }
 
   return (
     <div className="App">
       <LoadingBar className={classes.loadingBar} />
-      <Nav />
-      {loading !== 0 ? null :
-        <Container maxWidth='sm'>
-          <Switch>
-            <Route exact path='/'>
-              <Dashboard />
-            </Route>
-            <Route path='/add'>
-              <NewQuestion />
-            </Route>
-            <Route path='/leaderboard'>
-              <Leaderboard />
-            </Route>
-            <Route exact path='/questions/:questionId'>
-              <CardTemplate component={QuestionPoll} />
-            </Route>
-            <Route path='/questions/:questionId/results'>
-              <CardTemplate component={QuestionResults} />
-            </Route>
-            <Route path='/login'>
-              <Login />
-            </Route>
-          </Switch>
-        </Container>
-      }
+      <Nav loggedIn={loggedIn} />
+      <Container maxWidth='sm'>
+        <Switch>
+          <Route exact path='/'>
+            <Dashboard />
+          </Route>
+          <Route path='/add'>
+            <NewQuestion />
+          </Route>
+          <Route path='/leaderboard'>
+            <Leaderboard />
+          </Route>
+          <Route exact path='/questions/:questionId'>
+            <CardTemplate component={QuestionPoll} />
+          </Route>
+          <Route path='/questions/:questionId/results'>
+            <CardTemplate component={QuestionResults} />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
+      </Container>
     </div>
   );
 }

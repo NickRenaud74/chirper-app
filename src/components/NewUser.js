c2f8da8b782237cf1d359d379574512f9c8f6a44
Nick Renaud <nick_renaud@hotmail.com>
2021-02-04 18:49:08 -0800

    feat: Create a new user component
    -allow user to create new profile with first and last name
    - can upload their own image to use for an avatar

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,22 +1,93 @@
-import React from 'react'
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Input} from '@material-ui/core'
+import React, {useState} from 'react'
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Input, IconButton, Avatar, Button, TextField, Tooltip } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { useStyles } from '../styles/cardStyles'
+import { saveCreateUser } from '../actions/users'
+import { setAuthedUser } from '../actions/authedUser'
+import { useHistory } from 'react-router-dom'
 
 
 function NewUser() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [avatarUrl, setAvatarUrl] = useState('')
+    const [toHome, setToHome] = useState(false)
     const classes = useStyles()
+    const dispatch = useDispatch()
+    const history = useHistory()
+    
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        if (name === 'firstName') return setFirstName(value)
+        else if (name === 'lastName') return setLastName(value)
+        else if (name === 'avatarUrl') {
+            const reader = new FileReader()
+            const file = e.target.files[0]
+            reader.onloadend = event => setAvatarUrl(event.target.result)
+            reader.readAsDataURL(file)
+        } 
+    }
+
+    function formatUser(fName, lName) {
+        const name = `${fName[0].toUpperCase() + fName.slice(1)} ${lName[0].toUpperCase() + lName.slice(1)}`
+        const id = fName.toLowerCase().concat(lName.toLowerCase())
+        return {name, id}
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const { name, id } = formatUser(firstName, lastName)
+        dispatch(saveCreateUser({ name, id, avatarUrl }))
+        dispatch(setAuthedUser(id))
+        setToHome(id ? true : false)
+    }
+
+    console.log(toHome)
+    toHome && history.push('/')
 
     return (
         <Accordion>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}  >
                 <Typography variant='subtitle1' className={classes.centerText}>Create A New Account</Typography>
             </AccordionSummary>
-            <AccordionDetails >
-                <Input required placeholder='First Name' className={ classes.form}/>
-                <Input required placeholder='Last Name' className={classes.form} />
-            </AccordionDetails>
-            <Typography variant='subtitle1' className={classes.centerText}>Pick An Avatar</Typography>
+            <form onSubmit={handleSubmit}>
+                <AccordionDetails className={classes.createUser}>
+                    <TextField
+                        required
+                        placeholder='First Name'
+                        className={classes.form}
+                        name='firstName'
+                        value={firstName}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        required
+                        placeholder='Last Name'
+                        className={classes.form}
+                        name='lastName'
+                        value={lastName}
+                        onChange={handleChange}
+                    />
+                    <Input
+                        accept='image/*'
+                        type='file'
+                        id='avatar-button-file'
+                        style={{ display: 'none' }}
+                        name='avatarUrl'
+                        onChange={handleChange}
+                    />
+                    <label htmlFor='avatar-button-file'>
+                        <Tooltip title='Upload your avatar' arrow placement='top-end'>
+                            <IconButton aria-label='upload picture' component='span'>
+                                <Avatar src={avatarUrl} className={classes.avatarMed}></Avatar>
+                            </IconButton>
+                        </Tooltip>
+                    </label>
+                </AccordionDetails>
+                <Button type='submit' className={classes.button}>Create User</Button>
+            </form>
         </Accordion>
     )
 }

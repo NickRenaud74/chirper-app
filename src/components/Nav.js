2259ade8b987bca6705b5024a473d3a0f56e7bb1
Nick Renaud <nick_renaud@hotmail.com>
2021-02-04 18:48:25 -0800

    feat: add handleClick to log user in/out

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { setAuthedUser } from '../actions/authedUser'
 import { Tab, Tabs, AppBar } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
-function Nav() {
+function Nav({loggedIn}) {
     const [tab, setTab] = useState(0)
-    const authedUser = useSelector(state => state.authedUser)
-
-    const loggedIn = authedUser !== null
+    const dispatch = useDispatch()
+    const history = useHistory()
 
     function handleChange(e, newValue) {
         setTab(newValue)
     }
+
+    function handleClick() {
+        if (loggedIn) {
+            dispatch(setAuthedUser(null))
+            history.push('/login')
+        }
+    }
+
     return (
         <AppBar color='primary' position='static'>
             <Tabs
@@ -22,7 +31,7 @@ function Nav() {
                 <Tab label='Home' component={Link} to='/' />
                 <Tab label='New Question' component={Link} to='/add' />
                 <Tab label='Leaderboard' component={Link} to='/leaderboard' />
-                <Tab label={loggedIn ? 'Logout': 'Login'} component={Link} to='/Login' />
+                <Tab label={loggedIn ? 'Logout': 'Login'} onClick={handleClick} />
             </Tabs>
         </AppBar>
     )

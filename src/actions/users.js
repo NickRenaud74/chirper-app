a94493562ed9149b5efd77a2a9a56e2da7980fe1
Nick Renaud <nick_renaud@hotmail.com>
2021-02-04 18:44:18 -0800

    feat: add createUser actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,9 +1,27 @@
+import { _saveNewUser } from '../utils/_DATA'
+
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const CREATE_USER = 'CREATE_USER'
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+function createUser(user) {
+    return {
+        type: CREATE_USER,
+        user
+    }
+}
+
+export function saveCreateUser(user) {
+    return async dispatch => {
+        await _saveNewUser(user)
+        dispatch(createUser(user))
+        
     }
 }
 

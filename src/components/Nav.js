308be3ebd8d34b5ed005053ac3ab252882a623c0
Nick Renaud <nick_renaud@hotmail.com>
2021-02-07 13:27:32 -0800

    refactor: for user to log in or out
    -allow logIn and out button in the navbar
    -display username and avatar
    -update display of navItems

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { setAuthedUser } from '../actions/authedUser'
-import { Tab, Tabs, AppBar } from '@material-ui/core';
+import { Tab, Tabs, AppBar, Avatar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom'
+import {useStyles} from '../styles/styles'
 
-function Nav({loggedIn}) {
+function Nav({authedUser}) {
     const [tab, setTab] = useState(0)
+    const users = useSelector(state => state.users)
     const dispatch = useDispatch()
     const history = useHistory()
+    const classes = useStyles()
+
+    const loggedIn = authedUser !== null
 
     function handleChange(e, newValue) {
         setTab(newValue)
@@ -23,16 +28,28 @@ function Nav({loggedIn}) {
 
     return (
         <AppBar color='primary' position='static'>
+            <Toolbar >
             <Tabs
-                variant='fullWidth'
+                variant='standard'
                 value={tab}
-                onChange={handleChange}
+                    onChange={handleChange}
+                style={{flex: 1}}
             >
                 <Tab label='Home' component={Link} to='/' />
                 <Tab label='New Question' component={Link} to='/add' />
                 <Tab label='Leaderboard' component={Link} to='/leaderboard' />
-                <Tab label={loggedIn ? 'Logout': 'Login'} onClick={handleClick} />
-            </Tabs>
+                </Tabs>
+                {loggedIn ?
+                    <Box style={{display: 'flex'}}>
+                        <Avatar className={classes.avatarXs} src={users[authedUser].avatarURL} />
+                        <Typography variant='subtitle2'>
+                        {`Welcome, ${users[authedUser].name}`}
+                    </Typography>
+                    </Box>
+                    : null
+                }
+                <Tab label={loggedIn ? `Logout` : 'Login'} onClick={handleClick} />
+                </Toolbar>
         </AppBar>
     )
 }

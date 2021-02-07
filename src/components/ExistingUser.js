43a7eec67c27ad544b1f30f848d60b48076177a3
Nick Renaud <nick_renaud@hotmail.com>
2021-02-07 13:20:52 -0800

    feat: turn into accordion
    -extract existing user form from the login page and create an accordion
    component

---
+++ src/components/ExistingUser.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Select, MenuItem, ListItemText, Button } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { useStyles } from '../styles/styles'
+
+function ExistingUser({ setToDashboard, handleExpand, expanded }) {
+    const [userId, setUserId] = useState('')
+    const users = useSelector(state => state.users)
+    const dispatch = useDispatch()
+    const classes = useStyles()
+
+    function handleChange(e) {
+        setUserId(e.target.value)
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        await dispatch(setAuthedUser(userId))
+        setToDashboard(true)
+    }
+
+    return (
+        <Accordion expanded={expanded === 'panel1'} onChange={handleExpand('panel1')} >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant='h6' className={classes.centerText}>Sign In As Existing User</Typography>
+            </AccordionSummary>
+            <AccordionDetails style={{ justifyContent: 'center' }} >
+                <form onSubmit={handleSubmit} className={classes.loginForm}>
+                    <Select
+                        displayEmpty
+                        required
+                        value={userId}
+                        onChange={handleChange}
+                        className={classes.loginForm}
+                    >
+                        <MenuItem value='' disabled>Select User</MenuItem>
+                        {Object.keys(users).map(user => (
+                            <MenuItem key={user} value={users[user].id}>
+                                <ListItemText>{users[user].name}</ListItemText>
+                            </MenuItem>
+                        ))}
+                    </Select>
+                    <Button type='submit' className={classes.button}>
+                        Login
+                    </Button>
+                </form>
+            </AccordionDetails>
+        </Accordion>
+    )
+}
+
+export default ExistingUser

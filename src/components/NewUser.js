de6eb177929913fae588e6f0d05fd1c8b71d6863
Nick Renaud <nick_renaud@hotmail.com>
2021-02-07 13:25:18 -0800

    refactor: for private routing and accordion
    -controlled accordion features to expand
    -allow for redirecting after creating user

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,57 +1,54 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Accordion, AccordionSummary, AccordionDetails, Typography, Input, IconButton, Avatar, Button, TextField, Tooltip } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import { useStyles } from '../styles/cardStyles'
+import { useStyles } from '../styles/styles'
 import { saveCreateUser } from '../actions/users'
 import { setAuthedUser } from '../actions/authedUser'
-import { Redirect } from 'react-router-dom'
 
 
-function NewUser() {
+function NewUser({ setToDashboard, handleExpand, expanded }) {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
-    const [avatarUrl, setAvatarUrl] = useState('')
-    const [toDashboard, setToDashboard] = useState(false)
+    const [avatarURL, setAvatarURL] = useState('')
     const classes = useStyles()
     const dispatch = useDispatch()
-    
+
 
     function handleChange(e) {
         const { name, value } = e.target
         if (name === 'firstName') return setFirstName(value)
         else if (name === 'lastName') return setLastName(value)
-        else if (name === 'avatarUrl') {
+        else if (name === 'avatarURL') {
             const reader = new FileReader()
             const file = e.target.files[0]
-            reader.onloadend = event => setAvatarUrl(event.target.result)
+            reader.onloadend = event => setAvatarURL(event.target.result)
             reader.readAsDataURL(file)
-        } 
+        }
     }
 
     function formatUser(fName, lName) {
         const name = `${fName[0].toUpperCase() + fName.slice(1)} ${lName[0].toUpperCase() + lName.slice(1)}`
         const id = (fName + lName).toLowerCase()
-        return {name, id}
+        return { name, id }
     }
 
     async function handleSubmit(e) {
         e.preventDefault()
         const { name, id } = formatUser(firstName, lastName)
-        await dispatch(saveCreateUser({ name, id, avatarUrl }))
+        await dispatch(saveCreateUser({ name, id, avatarURL }))
         await dispatch(setAuthedUser(id))
         setToDashboard(true)
     }
 
-    toDashboard && <Redirect to='/' />
-        
+
     return (
-        <Accordion>
+        <Accordion expanded={expanded === 'panel2'} onChange={handleExpand('panel2')}>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}  >
-                <Typography variant='subtitle1' className={classes.centerText}>Create A New Account</Typography>
+                <Typography variant='h6' className={classes.centerText}>Create A New Account</Typography>
             </AccordionSummary>
             <form onSubmit={handleSubmit}>
-                <AccordionDetails className={classes.createUser}>
+                <AccordionDetails style={{ alignItems: 'center' }}>
                     <TextField
                         required
                         placeholder='First Name'
@@ -73,18 +70,18 @@ function NewUser() {
                         type='file'
                         id='avatar-button-file'
                         style={{ display: 'none' }}
-                        name='avatarUrl'
+                        name='avatarURL'
                         onChange={handleChange}
                     />
                     <label htmlFor='avatar-button-file'>
                         <Tooltip title='Upload your avatar' arrow placement='top-end'>
                             <IconButton aria-label='upload picture' component='span'>
-                                <Avatar src={avatarUrl} className={classes.avatarMed}></Avatar>
+                                <Avatar src={avatarURL} className={classes.avatarMed}></Avatar>
                             </IconButton>
                         </Tooltip>
                     </label>
                 </AccordionDetails>
-                <Button type='submit' className={classes.button}>Create User</Button>
+                <Button type='submit' className={`${classes.loginForm} ${classes.button}`}>Create User</Button>
             </form>
         </Accordion>
     )

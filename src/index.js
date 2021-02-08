27c7297105125794b51f19abf65effc519cac3da
Nick Renaud <nick_renaud@hotmail.com>
2021-02-08 13:35:56 -0800

    add stylesprovider

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
-import {BrowserRouter} from 'react-router-dom'
-import './styles/index.css';
-import './styles/styles'
+import { BrowserRouter } from 'react-router-dom'
+import './styles/index.css'
 import { theme } from './styles/theme'
-import {ThemeProvider} from '@material-ui/core'
-import App from './components/App';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles'
+import App from './components/App'
 
 const store = createStore(reducer, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <StylesProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </StylesProvider>
   </Provider>,
   document.getElementById('root')
 );
